c6bef73009d64e43599a1410e1a1d1a579351c56
91994 <suddala.rajukumar123@gmail.com>
2022-10-15 08:26:29 +0530

    updating

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -4,7 +4,7 @@ source  = "terraform-aws-modules/eks/aws"
   version = "18.26.6"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.23"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
